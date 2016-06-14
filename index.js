f430d0c64ae6dc83c58e809a05bf9bc266339025
plaza-s <plaza-s@mgdis.fr>
2016-06-14 22:10:15 +0200

    fix: arrays patches

--- index.js
+++ index.js
@@ -1,29 +1,85 @@
 var toDot = require('jsonpath-to-dot');
+var _ = require('lodash');
 
-module.exports = function(patches){
-  var update = {};
-  patches.map(function(p){
-    if(p.op === 'add'){
-      var path = toDot(p.path);
-      if(!update.$push) update.$push = {};
-      if(!update.$push[path]) {
-        update.$push[path] = p.value;
-      } else if (update.$push[path]) {
-        if(!update.$push[path].$each) {
-          update.$push[path] = {$each : [update.$push[path]]};
-        }
-        update.$push[path].$each.push(p.value);
+function _isInt(value) {
+  var x;
+  return isNaN(value) ? !1 : (x = parseFloat(value), (0 | x) === x);
+}
+
+function _set(update, path, p) {
+
+  if (!update.$set) {
+    update.$set = {};
+  }
+
+  update.$set[toDot(p.path)] = p.value;
+}
+
+function _push(update, path, p, index) {
+
+  if (!update.$push) {
+    update.$push = {};
+  }
+
+  if (!update.$push[path]) {
+    update.$push[path] = {
+      $each: [p.value]
+    };
+    if (index) {
+      update.$push[path].$position = index;
+    }
+
+  } else if (update.$push[path]) {
+    if (!update.$push[path].$each) {
+      update.$push[path] = {
+        $each: [update.$push[path]]
+      };
+
+      if (index) {
+        update.$push[path].$position = index;
       }
     }
-    else if(p.op === 'remove'){
-      if(!update.$unset) update.$unset = {};
+    update.$push[path].$each.push(p.value);
+  }
+}
+
+module.exports = function(patches) {
+  var update = {};
+  patches.map(function(p) {
+    if (p.op === 'add') {
+      var path = toDot(p.path);
+
+      var lastNode = _.chain(path)
+        .split('.')
+        .last()
+        .value();
+
+      var lastNodeParsed = parseInt(lastNode, 10);
+
+      var isArray =  lastNode === '-' || _isInt(lastNodeParsed);
+
+      if (isArray) {
+        var newPath = _.chain(path)
+          .split('.')
+          .initial()
+          .join('.')
+          .value();
+
+        _push(update, newPath, p, lastNodeParsed);
+      } else {
+        _set(update, path, p);
+      }
+    } else if (p.op === 'remove') {
+      if (!update.$unset) {
+        update.$unset = {};
+      }
       update.$unset[toDot(p.path)] = 1;
-    }
-    else if(p.op === 'replace'){
-      if(!update.$set) update.$set = {};
+    } else if (p.op === 'replace') {
+      if (!update.$set) {
+        update.$set = {};
+      }
       update.$set[toDot(p.path)] = p.value;
-    }
-    else if(p.op !== 'test') {
+    } else if (p.op !== 'test') {
       throw new Error('Unsupported Operation! op = ' + p.op);
     }
   });
